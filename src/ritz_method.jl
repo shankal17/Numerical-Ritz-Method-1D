b9803af07765e70b9abf6c3b057c370df878dca9
Austin Shank <shankaustin17@gmail.com>
2026-08-05 21:03:42 -0700

    Wrote ritz function

--- src/ritz_method.jl
+++ src/ritz_method.jl
@@ -3,10 +3,13 @@ module ritz_method
 using ArgCheck
 using ForwardDiff: derivative
 using FastGaussQuadrature: gausslegendre
-import LinearAlgebra
+using LinearAlgebra: dot
+using ADTypes: AutoForwardDiff
+using Optim: optimize, BFGS
 
-export integrate_function, Interval
+export Interval, ritz
 
+# TODO: Add docstrings
 struct Interval
     low::Float64
     high::Float64
@@ -17,56 +20,45 @@ struct Interval
 
 end
 
-function integrate_function(f::Function, domain::Interval, num_nodes::Int)
-    @argcheck num_nodes > 0
-    nodes, weights = gausslegendre(num_nodes)
+# TODO: Add docstrings
+function ritz(
+    F::Function,
+    domain::Interval,
+    phi_s::Vector{<:Function};
+    phi_0::Function = x-> 0.0,
+    num_quad_nodes::Int = 15,
+    initial_coeffs::Vector{Float64} = zeros(length(phi_s))
+)
+    @argcheck length(phi_s) == length(initial_coeffs)
+    @argcheck num_quad_nodes > 0
+
+    nodes, weights = gausslegendre(num_quad_nodes)
 
     # Change of interval
-    coeff = (domain.high - domain.low) / 2
+    coeff = (domain.high - domain.low) / 2 # Also coefficient that is applied during quadrature
     bias = (domain.low + domain.high) / 2
     new_nodes = map(x_i -> coeff * x_i + bias, nodes)
 
-    # Quadrature
-    res = coeff * LinearAlgebra.dot(weights, f.(new_nodes))
+    # Precompute basis functions and their derivatives at nodes
+    phi_0_vals = map(x -> phi_0(x), new_nodes)
+    phi_i_vals = [phi(x) for x in new_nodes, phi in phi_s]
+
+    phi_0_prime_vals = map(x -> derivative(phi_0, x), new_nodes)
+    phi_i_prime_vals = [derivative(phi, x) for x in new_nodes, phi in phi_s]
+
+    function objective(basis_coeffs)
+        y_vals = phi_0_vals .+ phi_i_vals * basis_coeffs
+        y_prime_vals = phi_0_prime_vals .+ phi_i_prime_vals * basis_coeffs
+        F_vals = F.(new_nodes, y_vals, y_prime_vals)
+
+        return coeff * dot(weights, F_vals)
+    end
+
+    # Optimize
+    res = optimize(objective, initial_coeffs, BFGS(); autodiff = AutoForwardDiff())
 
     return res
 end
 
-# TODO: I'm pretty sure that we don't need to evaluate phi's so much, cause we're optimizing over coeffs, and the values of basis
-# functions don't change per opimization pass
-function eval_y_bar(x::Float64, coeffs::Vector{Float64}, phi_0::Function, phi_s::Vector{Function})::Float64
-    val = phi_0(x)
-    for i in eachindex(c)
-        val += coeffs[i] * phi_s[i](x)
-    end
-
-    return val
-end
-
-function eval_y_bar_prime(x::Float64, coeffs::Vector{Float64}, phi_0::Function, phi_s::Vector{Function})::Float64
-    return ForwardDiff.derivative(x_val -> eval_y_bar(x_val, coeffs, phi_0, phi_s), x)
-end
-
-function create_objective(
-    integrand::Function,
-    domain::Interval,
-    phi_s::Vector{Function},
-    phi_0::Function = x-> 0.0,
-    num_quad_nodes::Int = 15,
-    initial_coeffs::Vector{Float64} = zeros(length(phi_s))
-)
-
-end
-
-function ritz(
-    integrand::Function,
-    domain::Interval,
-    phi_s::Vector{Function},
-    phi_0::Function = x-> 0.0,
-    num_quad_nodes::Int = 15,
-    initial_coeffs::Vector{Float64} = zeros(length(phi_s))
-)
-
-end
 
 end # module ritz_method
