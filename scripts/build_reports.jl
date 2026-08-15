89cf368600b44c7ce6b22dbe919c583aadb5f7e1
Austin Shank <shankaustin17@gmail.com>
2026-08-15 13:34:02 -0700

    Updated reports + added pdf compiled report

--- scripts/build_reports.jl
+++ scripts/build_reports.jl
@@ -1,8 +1,11 @@
 using Weave
 
+const PDF_TEMPLATE = joinpath(@__DIR__, "pdf_template.tpl")
+
 for dir in filter(isdir, readdir("scripts"; join=true))
     jmd = joinpath(dir, "report.jmd")
     isfile(jmd) || continue
     weave(jmd; doctype="md2html", out_path=joinpath(dir, "report"))
+    weave(jmd; doctype="md2pdf", out_path=joinpath(dir, "report"), template=PDF_TEMPLATE)
     @info "Wove $jmd"
 end
